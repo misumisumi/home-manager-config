9b2ef72e057923a5653914ebfe8f1bebffe3e4a5
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-22 01:11:30 +0900

    update(flake): update & remove input

--- flake.nix
+++ flake.nix
@@ -1,36 +1,9 @@
 {
   description = "Each my machine NixOS System Flake Configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    nur.url = "github:nix-community/NUR";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flakes = {
-      url = "github:misumisumi/flakes";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nvimdots = {
-      # url = "github:misumisumi/nvimdots";
-      url = "github:misumisumi/nvimdots";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flakes.follows = "flakes";
-    };
-  };
+  inputs = {};
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    flake-utils,
-    nur,
-    home-manager,
-    flakes,
-    nvimdots,
-  }: {
+  outputs = inputs: {
     nixosModules = {
       for-nixos = import ./modules/for-nixos.nix;
       for-hm = import ./modules/for-hm.nix;
