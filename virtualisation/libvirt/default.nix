5f0984c4518f8847fbdc248943a632661779ed57
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-22 19:13:48 +0900

    fix: install some utils for virtualisation to system wide

--- virtualisation/libvirt/default.nix
+++ virtualisation/libvirt/default.nix
@@ -1,42 +1,47 @@
 /*
 libvirt conf
 */
-{ pkgs, user, ... }:
-
 {
+  pkgs,
+  user,
+  ...
+}: {
   environment = {
     systemPackages = with pkgs; [
       win-virtio
+      virt-manager
+      looking-glass-client
+      dmidecode # Show BIOS info
     ];
   };
   users = {
     groups = {
       libvirt = {
-        members = [ "root" "${user}" ];
+        members = ["root" "${user}"];
       };
     };
   };
 
   virtualisation = {
     libvirtd = {
-      enable = true;                          # Virtual drivers
+      enable = true; # Virtual drivers
       onBoot = "ignore";
       onShutdown = "shutdown";
       qemu = {
         ovmf = {
           enable = true;
-          packages = with pkgs; [ pkgs.OVMFFull.fd pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd ];
+          packages = with pkgs; [pkgs.OVMFFull.fd pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd];
         };
         swtpm.enable = true;
         # verbatimConfig = ''
-        #   nvram = [ 
+        #   nvram = [
         #     "${pkgs.OVMFFull}/FV/OVMF.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
         #     "${pkgs.OVMFFull}/FV/OVMF.secboot.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
         #   ]
         # '';
       };
     };
-    spiceUSBRedirection.enable = true;        # USB passthrough
+    spiceUSBRedirection.enable = true; # USB passthrough
     sharedMemoryFiles = {
       looking-glass = {
         user = "${user}";
