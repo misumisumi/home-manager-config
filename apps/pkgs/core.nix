3a881ea8e6dacc588334e6c16179aca1bea1f0c3
misumisumi <dragon511southern@gmail.com>
2023-10-07 18:58:01 +0900

    fix(pkgs): Some package move to extra

--- apps/pkgs/core.nix
+++ apps/pkgs/core.nix
@@ -9,24 +9,14 @@ in
 with pkgs;
 [
   (python3.withPackages pythonPkgs)
-  nix-index # A files database for nixpkgs
-  nix-prefetch # Prefetch checkers
-  nix-prefetch-git
-  nvfetcher # Tool of automate nix package updates
 
-  ascii-image-converter # Make ascii art
   bc # GNU calculater
   bintools # Manipulating binaries
   bottom # System monitor
   btop # System monitor
-  cmatrix # Lain of character
-  convmv #convert encoding
-  devbox # Instant, easy, predictable shells and container
   dig # Domain name server
   duf # Show storage usage
   fd # fast find
-  ffmpeg # Multi media solution
-  figlet # Make AA from character
   iperf3 # Network speed test tool
   jq # JSON processor
   lsof # check port
