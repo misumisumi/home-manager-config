e24465bd3c01ebaa0882cd094f6c4cd022559d76
misumisumi <dragon511southern@gmail.com>
2023-11-08 19:11:49 +0900

    update: weztermのsshの設定がある場合に読み込む

--- apps/wezterm/config/wezterm.lua
+++ apps/wezterm/config/wezterm.lua
@@ -4,5 +4,10 @@ local keymap = require("keymap")
 
 appearance = utils.merge_tables(appearance, keymap)
 
+local ok, ssh_config = pcall(require, "ssh")
+if ok then
+    appearance = utils.merge_tables(appearance, ssh_config)
+end
+
 -- return utils.merge_tables(appearance, keymap)
 return appearance
