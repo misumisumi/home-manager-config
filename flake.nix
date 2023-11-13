88b1850c5f8a5717d909b0a059b386df9d83eae1
misumisumi <dragon511southern@gmail.com>
2023-11-13 15:00:25 +0900

    add: homeConfigulartion

--- flake.nix
+++ flake.nix
@@ -1,9 +1,67 @@
 {
-  description = "My common dotfiles";
+  description = "My home-manager config";
 
-  outputs = inputs: {
-    nixosModules = {
-      home-manager = import ./modules/home-manager.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nur.url = "github:nix-community/NUR";
+    musnix = {
+      url = "github:musnix/musnix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixgl = {
+      url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-matlab = {
+      url = "gitlab:doronbehar/nix-matlab";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nvimdots.url = "github:misumisumi/nvimdots";
+    flakes.url = "github:misumisumi/flakes";
+    private-config = {
+      url = "git+ssh://git@github.com/misumisumi/nixos-private-config.git";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = inputs:
+    let
+      stateVersion = "23.11";
+      overlay =
+        { system }:
+        let
+          nixpkgs-stable = import inputs.nixpkgs-stable {
+            inherit system;
+            config = { allowUnfree = true; };
+          };
+        in
+        {
+          nixpkgs.overlays =
+            [
+              inputs.nur.overlay
+              inputs.nixgl.overlay
+              inputs.nix-matlab.overlay
+              inputs.flakes.overlays.default
+              inputs.private-config.overlays.default
+            ]
+            ++ (import ./patches { inherit nixpkgs-stable; });
+        };
+    in
+    {
+      nixosModules = {
+        home-manager = import ./modules/home-manager.nix;
+      };
+      homeConfigurations = (
+        import ./hosts {
+          inherit (inputs.nixpkgs) lib;
+          inherit inputs overlay stateVersion;
+        }
+      );
+    };
 }
---
+++ hosts/default.nix
@@ -0,0 +1,37 @@
+{ inputs
+, overlay
+, stateVersion
+, ...
+}:
+let
+  settings =
+    { hostname
+    , user
+    , system ? "x86_64-linux"
+    , withExtra ? false
+    , withGui ? false
+    , homeDirectory ? ""
+    }:
+    let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      inherit (inputs.nixpkgs) lib;
+    in
+    inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = { inherit hostname user stateVersion withExtra withGui homeDirectory; };
+      modules = [
+        (overlay { inherit system; })
+        inputs.nur.nixosModules.nur
+
+        inputs.flakes.nixosModules.for-hm
+        inputs.nvimdots.nixosModules.nvimdots
+        ./modules/home-manager
+      ]; # ++ lib.optional withGui ./gui.nix;
+    };
+in
+{
+  cui = settings {
+    hostname = "cui";
+    user = "hm-user";
+  };
+}
