83413e96cff47d71ac6ee26c94206c71b60998d1
misumisumi <dragon511southern@gmail.com>
2023-11-20 18:07:23 +0900

    feat: config for sops

--- apps/minimal/ssh/default.nix
+++ apps/minimal/ssh/default.nix
@@ -1,10 +1,21 @@
 {
-  imports = [ ./matchblock.nix ];
   programs.ssh = {
     enable = true;
     forwardAgent = true;
     serverAliveInterval = 30;
     serverAliveCountMax = 5;
+    includes = [ "common/ssh/conf.d/hosts/*" ];
+    matchBlocks = {
+      "github" = {
+        user = "git";
+        hostname = "github.com";
+      };
+      "github.com" = {
+        user = "git";
+        hostname = "ssh.github.com";
+        port = 443;
+      };
+    };
   };
 }
 
