8a90c28e822ee12a62274f3a8feb40f0a012a2e2
misumisumi <dragon511southern@gmail.com>
2024-01-23 12:22:18 +0900

    fix: auto generate age keyfile from ssh-key

--- apps/core/sops/default.nix
+++ apps/core/sops/default.nix
@@ -1,5 +1,15 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
 {
+  home.activation.ageActivatioinAction = lib.hm.dag.entryAfter [ "sshActivatioinAction" ] ''
+    if [ ! -d ${config.xdg.configHome}/.config/sops/age ]; then
+      ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i ${config.home.homeDirectory}/.ssh/id_ed25519 > ${config.xdg.configHome}/sops/age/keys.txt
+    fi
+  '';
   sops = {
-    age.generateKey = true;
+    age.generateKey = false;
   };
 }
