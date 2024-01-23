b2878eee2357ee45cd22c796806873149915ae07
misumisumi <dragon511southern@gmail.com>
2024-01-23 11:42:14 +0900

    ssh: fix activation script

--- apps/core/ssh/default.nix
+++ apps/core/ssh/default.nix
@@ -6,8 +6,12 @@
 }:
 {
   home.activation.sshActivatioinAction = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    [[ ! $(find ~/.ssh -type f | grep "ed25519") ]] && ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -N "" -C "$(whoami)@${hostname}-$(date -I)" -f ${config.home.homeDirectory}/.ssh/id_ed25519
-    [[ ! $(find ~/.ssh -type f | grep "rsa") ]] && ${pkgs.openssh}/bin/ssh-keygen -t rsa -N "" -C "$(whoami)@${hostname}-$(date -I)" -f ${config.home.homeDirectory}/.ssh/id_rsa
+    if [ ! $(find ~/.ssh -type f | grep "ed25519") ]; then
+      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -N "" -C "$(whoami)@${hostname}-$(date -I)" -f ${config.home.homeDirectory}/.ssh/id_ed25519
+    fi
+    if [ ! $(find ~/.ssh -type f | grep "rsa") ]; then
+      ${pkgs.openssh}/bin/ssh-keygen -t rsa -N "" -C "$(whoami)@${hostname}-$(date -I)" -f ${config.home.homeDirectory}/.ssh/id_rsa
+    fi
   '';
   programs.ssh = {
     enable = true;
