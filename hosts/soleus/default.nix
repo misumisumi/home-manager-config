ad750dcf7dc860870e2a45e5e979e1eb860b0a0c
misumisumi <dragon511southern@gmail.com>
2024-01-23 11:48:13 +0900

    soleus: add some gui apps

--- hosts/soleus/default.nix
+++ hosts/soleus/default.nix
@@ -2,11 +2,17 @@
 , ...
 }:
 {
+  imports = [
+    ../../apps/full/programs/firefox.nix
+    ../../apps/full/programs/vivaldi.nix
+  ] ++ (import ../../apps/full/ime);
   home = {
     packages = with pkgs;[
       ffmpeg # Multi media solution
       graphicsmagick # CLI Image Editor
+      nomacs # Image Viewer
       sox # CLI Sound Editor
+      wavesurfer # pkgs from Sumi-Sumi/flakes
     ];
   };
 }
