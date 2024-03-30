46dcaf76dbf6a4433377c1513c56c8fc516c96e4
misumisumi <dragon511southern@gmail.com>
2024-03-31 01:07:24 +0900

    fix(wezterm): remove <C-P> because use neovim

--- apps/core/wezterm/wezterm/keymap.lua
+++ apps/core/wezterm/wezterm/keymap.lua
@@ -188,7 +188,6 @@ local config = {
         { key = "k", mods = "SHIFT|CTRL", action = act.ActivatePaneDirection("Up") },
         { key = "l", mods = "SHIFT|CTRL", action = act.ActivatePaneDirection("Right") },
         { key = "o", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
-        { key = "p", mods = "SHIFT|CTRL", action = act.ShowLauncher },
         { key = "q", mods = "CTRL|LEADER", action = act.QuickSelect },
         { key = "R", mods = "SHIFT|CTRL", action = act.ReloadConfiguration },
         { key = "s", mods = "CTRL|SHIFT", action = act.PaneSelect({ alphabet = "", mode = "SwapWithActive" }) },
